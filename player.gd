1e384bb0dfe01a0d69a887420058286d069c5605
Tom Chauvel <tom.chauvel@proton.me>
2024-04-06 01:27:44 +0200

    Tom (#4)
    * player update + object interaction
    
    * deadzone manette

--- player.gd
+++ player.gd
@@ -1,14 +1,25 @@
 extends CharacterBody2D
 
-
-const SPEED = 2000
-const FRICTION = 8
+const SPEED = 3500
+const FRICTION = 10
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","forward","backward")
+	
+	if direction.x != 0.0:
+		%Node2D.scale.x = int(direction.x > 0)*2 -1
 	
 	var acceleration = direction * SPEED \
 					 - velocity * FRICTION
 					
 	velocity += acceleration * delta
+	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("interact"):
+		var collisioned_objects = %ActionZone.get_overlapping_bodies()
+		if collisioned_objects.size() > 0:
+			for object in collisioned_objects:
+				if object.has_method("action"):
+					object.action()
+					break
