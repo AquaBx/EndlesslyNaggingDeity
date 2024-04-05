e1913fb012970dcb8717d7e174dbd8f2ce132c96
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2024-04-06 00:20:14 +0200

    implemented acceleration-based physics, constants can be set later

--- player.gd
+++ player.gd
@@ -1,6 +1,14 @@
 extends CharacterBody2D
 
+
+const SPEED = 2000
+const FRICTION = 8
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","forward","backward")
-	position += direction * 300 * delta
+	
+	var acceleration = direction * SPEED \
+					 - velocity * FRICTION
+					
+	velocity += acceleration * delta
 	move_and_slide()
