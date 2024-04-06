90b80d4bb0120bec0ebcad936a178bf89657511f
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2024-04-07 00:13:22 +0200

    added more interactable

--- scenes/hazards/door/Door.gd
+++ scenes/hazards/door/Door.gd
@@ -1,24 +1,33 @@
 extends StaticBody2D
 
 
-@export var player : CharacterBody2D
 signal player_dead
 
-func _on_body_entered(body):
+func _ready():
+	%Interact.visible = false
+	
+func glow():
+	%Interact.visible = true
+	
+func unglow():
+	%Interact.visible = false
+
+func action(player):
 	print("collided")
 	
-	body.frozen = true
+	player.frozen = true
 	await get_tree().create_timer(.5).timeout
+	unglow()
 	%DoorClosed.visible = false
 	%DoorOpen.visible = true
 	%BloodSplash.visible = true
-	body.visible = false
+	player.visible = false
 	
 	await get_tree().create_timer(2.).timeout
 	player_dead.emit("Tu est mort à cause d'une porte ! Dieu décida donc de détruire les portes.")
 	
 	await get_tree().create_timer(1).timeout
 	
-	body.frozen = false
-	body.visible = true
+	player.frozen = false
+	player.visible = true
 	queue_free()
