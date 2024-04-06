6e8c7d7f1513a544b457d9cb7a3c6f43ec4c26f4
Tom Chauvel <tom.chauvel@proton.me>
2024-04-06 18:04:43 +0200

    fixing malo's shit

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -3,21 +3,21 @@ extends Node2D
 func _ready():
 	%Player.position = %PlayerSpawn.position
 	%Player.frozen = false
-	%DeathScreen.show()
-	%DeathScreen.get_child(0).color = Color(0,0,0,0)
+	#%DeathScreen.get_child(0).color = Color(0,0,0,0)
+	%DeathLabel.hide()
 	
 func _on_player_death(death_message: String):
 	%Player.frozen = true
-	#%DeathLabel.text = death_message
+	%DeathLabel.text = death_message
 	
-	%DeathScreenAnimationPlayer.play("fade_in")	
+	%DeathScreenAnimationPlayer.play("fade in")	
 	
-	#%DeathScreen.show()
-	#await get_tree().create_timer(1.).timeout
-	#%DeathLabel.show()
-	#%DeathScreen.hide()
-	#%DeathLabel.hide()
-	#%DeathScreenAnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.).timeout
+	
+	%DeathLabel.show()
+	
+	await get_tree().create_timer(2.).timeout
+	%DeathScreenAnimationPlayer.play("fade out")
 	
 	%Player.position = %PlayerSpawn.position
 	%Player.frozen = false
