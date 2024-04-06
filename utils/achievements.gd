cff10a58387ee2f60d8821cc0874e2da499edd4c
Ota-cueille <63119937+Ota-cueille@users.noreply.github.com>
2024-04-06 08:48:03 +0200

    Vianney/achievements (#9)
    * working now
    
    * Achievement on RAM + Testing
    
    ---------
    
    Co-authored-by: Vianney HERVÉ <debota@winota>

---
+++ utils/achievements.gd
@@ -0,0 +1,20 @@
+extends Node
+
+class AchievementInfo:
+	var succeded := false
+	var description: String
+	var icon: String
+
+	func _init(desc: String, _icon: String):
+		self.description = desc
+		self.icon = _icon
+
+var achievements := {
+	"bontoutou": AchievementInfo.new("Vous avez réussi à obéir, félicitation !", "res://Assets/dino1.png")
+}
+
+func get_info(id: String) -> AchievementInfo:
+	return self.achievements[id]
+
+func set_success(id: String, val: bool) -> void:
+	self.achievements[id].succeded = val
