ced07355d1d6907cca788e60b085cece9f945db4
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2024-04-06 14:32:12 +0200

    door now freezes the player next to it

--- scenes/hazards/door/Door.gd
+++ scenes/hazards/door/Door.gd
@@ -4,6 +4,10 @@ extends Area2D
 @export var player : CharacterBody2D
 
 func _on_body_entered(body):
-	if player == body:
-		$"Door-placeholder".visible = false
-		$"Door-open-placeolder".visible = true
+	if player != body:
+		return
+	$"Door-placeholder".visible = false
+	$"Door-open-placeolder".visible = true
+	player.frozen = true
+	player.global_position = global_position + Vector2(-150, -60)
+	print(global_position, player.global_position)
