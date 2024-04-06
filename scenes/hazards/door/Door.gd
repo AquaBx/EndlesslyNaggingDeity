044c595ba4a6daadab1123f986a7ddd56e85f758
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2024-04-06 15:24:02 +0200

    added door and a blood splash

--- scenes/hazards/door/Door.gd
+++ scenes/hazards/door/Door.gd
@@ -6,8 +6,13 @@ extends Area2D
 func _on_body_entered(body):
 	if player != body:
 		return
+	
+	await get_tree().create_timer(.5).timeout
 	$"Door-placeholder".visible = false
 	$"Door-open-placeolder".visible = true
+	$"BloodSplash".visible = true
 	player.frozen = true
+	player.visible = false
 	player.global_position = global_position + Vector2(-150, -60)
 	print(global_position, player.global_position)
+	
