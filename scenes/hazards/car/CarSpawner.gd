ae453f546f87ac403ec1496d28908c342c028e3f
Malo | Astri <74017420+Astri2@users.noreply.github.com>
2024-04-07 01:33:40 +0200

    Door (#39)
    * refactored the door
    
    * tu bouge plus connard
    
    * freeze the player as soon as it touches the door
    
    * Added call to death
    
    * added interactable objects to the world
    
    * added and edited loupe
    
    * added more interactable
    
    * fixed plant actionability quirk, don't put it too close to the door
    
    * Plant now triggers the game loop
    
    * Added CarSpawner
    
    * refined the spawner and spaning points
    
    * micro commit
    
    * connect door signal to world
    
    ---------
    
    Co-authored-by: Aodren Le Gloanec <aodren.legloanec@gmail.com>

--- scenes/hazards/car/CarSpawner.gd
+++ scenes/hazards/car/CarSpawner.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @export var player : CharacterBody2D
+@export var CarSpawnWest : Marker2D
+@export var CarSpawnEast : Marker2D
+
 const SPAWNER_LENGTH = 100
 
 signal player_death
@@ -8,24 +11,19 @@ signal player_death
 func spawn_car():
 	var new_car = preload("res://scenes/hazards/car/car.tscn").instantiate()
 	new_car.get_node("Area2D").body_entered.connect(car_touched_player)
-	new_car.position = Vector2(0, 0)
-
 	
 	var dir = [-1, 1].pick_random()  # determine if the car goes to the left or right
-
 	new_car.direction = dir
 
 	var ctrans = get_canvas_transform()
 	if dir == 1:
-		new_car.position.y += 20
-		#new_car.global_position.x = player.global_position.x - 20
+		new_car.global_position = CarSpawnWest.global_position
 	else:
-		#new_car.global_position.x = player.global_position.x + 20
-		new_car.position.x += SPAWNER_LENGTH
+		new_car.global_position = CarSpawnEast.global_position
 		new_car.scale.x *= -1
-
 	add_child(new_car)
 
 func car_touched_player(body):
 	player_death.emit("les voitures c'est vraiment pô nice ! \n Ainsi Dieu Surpprima les voitures")
+	await get_tree().create_timer(2).timeout
 	queue_free()
