3d655feaecfb3074007abc4f0a4025549d9678ee
A1RONE <125598830+A1RONE@users.noreply.github.com>
2024-04-06 08:31:57 +0200

    DinoAnimation (#8)
    The dino is animated

--- player.gd
+++ player.gd
@@ -2,17 +2,25 @@ extends CharacterBody2D
 
 const SPEED = 3500
 const FRICTION = 10
+const SEUIL = 349
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","forward","backward")
 	
 	if direction.x != 0.0:
-		%Node2D.scale.x = int(direction.x > 0)*2 -1
+		%Dino.scale.x = int(direction.x > 0)*2 -1
 	
 	var acceleration = direction * SPEED \
 					 - velocity * FRICTION
 					
 	velocity += acceleration * delta
+	if velocity.length() < 10:
+		velocity = Vector2(0,0)
+	
+	if velocity.length() != 0 :
+		%Dino.play_walk_animation()
+	else :
+		%Dino.play_idle_animation()
 	
 	move_and_slide()
 	
