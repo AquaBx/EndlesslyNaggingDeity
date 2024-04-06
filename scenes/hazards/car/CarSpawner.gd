3e1f848ee08c8ecc010e1254ef0e40c33ac55b93
Malo | Astri <74017420+Astri2@users.noreply.github.com>
2024-04-06 21:56:09 +0200

    init

--- scenes/hazards/car/CarSpawner.gd
+++ scenes/hazards/car/CarSpawner.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 @export var player : CharacterBody2D
 const SPAWNER_LENGTH = 100
+const CAR: Resource = preload("res://scenes/hazards/car/car.tscn")
+var new_car
 
 signal player_death
 
 func spawn_car():
-	var new_car = preload("res://scenes/hazards/car/car.tscn").instantiate()
+	new_car = CAR.instantiate()
+
 	new_car.get_node("Area2D").body_entered.connect(car_touched_player)
 	new_car.position = Vector2(0, 0)
-	print(global_position, new_car.position)
 	
 	var dir = [-1, 1].pick_random()  # determine if the car goes to the left or right
-	print(dir)
 	new_car.direction = dir
 
 	var ctrans = get_canvas_transform()
@@ -23,9 +24,19 @@ func spawn_car():
 		#new_car.global_position.x = player.global_position.x + 20
 		new_car.position.x += SPAWNER_LENGTH
 		new_car.scale.x *= -1
-	print(new_car.global_position)
 	add_child(new_car)
 
 func car_touched_player(body):
+	new_car.car_speed = 0
+	play_car_death_animation()
+	await get_tree().create_timer(5).timeout
 	player_death.emit("les voitures c'est vraiment pô nice ! \n Ainsi Dieu Surpprima les voitures")
 	queue_free()
+
+const DINO_DOLL = preload("res://dino_doll.tscn")
+
+func play_car_death_animation():
+	player.get_node("Dino").hide()
+	var dino_doll = DINO_DOLL.instanciate()
+	add_sibling(dino_doll)
+	
