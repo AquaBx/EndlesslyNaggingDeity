212f0481b7b1ca1f4a46293720d7afd641edf8fd
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2024-04-06 21:54:43 +0200

    replaced placeholder textures for the door and implemented it in the spawn house

--- scenes/hazards/door/Door.gd
+++ scenes/hazards/door/Door.gd
@@ -1,17 +1,23 @@
-extends Area2D
+extends StaticBody2D
 
 
 @export var player : CharacterBody2D
+signal player_dead
 
 func _on_body_entered(body):
+	print("collided")
 	if player != body:
 		return
 	
 	await get_tree().create_timer(.5).timeout
-	$"Door-placeholder".visible = false
-	$"Door-open-placeolder".visible = true
-	$"BloodSplash".visible = true
+	%DoorClosed.visible = false
+	%DoorOpen.visible = true
+	%BloodSplash.visible = true
 	player.frozen = true
 	player.visible = false
-	print(global_position, player.global_position)
 	
+	await get_tree().create_timer(2.).timeout
+	player_dead.emit("Tu est mort à cause d'une porte ! Dieu décida donc de détruire les portes.")
+	
+	await get_tree().create_timer(1).timeout
+	queue_free()
