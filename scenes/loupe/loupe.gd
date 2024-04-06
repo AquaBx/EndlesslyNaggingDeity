b159c299b948daef451ba7b520aae8d23e45be56
Tom Chauvel <tom.chauvel@proton.me>
2024-04-06 20:58:06 +0200

    loupe

--- scenes/loupe/loupe.gd
+++ scenes/loupe/loupe.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+signal interactloupe
+
 func _ready():
 	%Interact.visible = false
 	$Node2D.play_animation_loop()
 	
 
 func action():
-	pass
+	interactloupe.emit("Il est mort avec une loupe, de ce fait, Dieu supprima la loupe !")
+	queue_free()
 	
 func glow():
 	%Interact.visible = true
