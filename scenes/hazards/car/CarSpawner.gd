fd06f892e2a800e9d48dc08e1586af3ac75c3865
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2024-04-07 00:49:47 +0200

    refined the spawner and spaning points

--- scenes/hazards/car/CarSpawner.gd
+++ scenes/hazards/car/CarSpawner.gd
@@ -11,11 +11,8 @@ signal player_death
 func spawn_car():
 	var new_car = preload("res://scenes/hazards/car/car.tscn").instantiate()
 	new_car.get_node("Area2D").body_entered.connect(car_touched_player)
-	new_car.position = Vector2(0, 0)
-	print(global_position, new_car.position)
 	
 	var dir = [-1, 1].pick_random()  # determine if the car goes to the left or right
-	print(dir)
 	new_car.direction = dir
 
 	var ctrans = get_canvas_transform()
@@ -23,9 +20,7 @@ func spawn_car():
 		new_car.global_position = CarSpawnWest.global_position
 	else:
 		new_car.global_position = CarSpawnEast.global_position
-		new_car.position.x += SPAWNER_LENGTH
 		new_car.scale.x *= -1
-	print(new_car.global_position)
 	add_child(new_car)
 
 func car_touched_player(body):
