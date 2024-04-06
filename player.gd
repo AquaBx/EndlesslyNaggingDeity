6b26160aa22b3266ccec2f24de5d04702e2a7df8
Tom Chauvel <tom.chauvel@proton.me>
2024-04-06 08:51:49 +0200

    glowing effect + refactor (#10)

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ const SPEED = 3500
 const FRICTION = 10
 const SEUIL = 349
 
+var m_object:Object
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","forward","backward")
 	
@@ -24,10 +26,14 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("interact"):
-		var collisioned_objects = %ActionZone.get_overlapping_bodies()
-		if collisioned_objects.size() > 0:
-			for object in collisioned_objects:
-				if object.has_method("action"):
-					object.action()
-					break
+	if Input.is_action_just_pressed("interact") and m_object:
+		m_object.action()
+
+func _on_action_zone_body_entered(body):
+	if body.has_method("action") and body.has_method("glow"):
+		m_object = body
+		m_object.glow()
+
+func _on_action_zone_body_exited(body):
+	m_object.unglow()
+	m_object = null
