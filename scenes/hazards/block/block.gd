461af4acbfd8097eaccda184e0582a651b69da51
Gwlanbzh <62286593+Gwlanbzh@users.noreply.github.com>
2024-04-07 03:51:16 +0200

    Block (#47)
    * implemented the cube
    
    * changed death message
    
    * fixed before broke
    
    ---------
    
    Co-authored-by: Tom Chauvel <tom.chauvel@proton.me>

---
+++ scenes/hazards/block/block.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+signal player_death
+
+func action(player):
+	%Glow.hide()
+	player.frozen = true
+	player.get_node("Dino").play_deathblock_animation()
+	await get_tree().create_timer(.25).timeout
+	%AnimationPlayer.play("mariojump")
+	
+	player_death.emit("Dieu n'aime pas que vous vous cassiez la tête.\nDieu a donc retiré les blocs bonus de ce monde.")
+	await get_tree().create_timer(1).timeout
+	queue_free()
+
+func glow():
+	%Glow.show()
+
+func unglow():
+	%Glow.hide()
