7b057d6f8261b19ccf776b6bbdd0191385109967
Malo | Astri <74017420+Astri2@users.noreply.github.com>
2024-04-07 02:25:36 +0200

    fix batiment outlines

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -6,7 +6,6 @@ func _ready():
 	#%DeathScreen.get_child(0).color = Color(0,0,0,0)
 	%DeathLabel.hide()
 	
-	
 func _on_player_death(death_message: String):
 	%Player.frozen = true
 	%DeathLabel.text = death_message
