521bdb3808b4b1b2e9384faa034ee7de3aa6a0e6
Tom Chauvel <tom.chauvel@proton.me>
2024-04-07 03:20:20 +0200

    fixed branch

--- scenes/player/doll/dino_doll.gd
+++ scenes/player/doll/dino_doll.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 var ejection_speed = 1
 
-func _process(delta):
+func _process(_delta):
 	%PathFollow2D.progress += ejection_speed
-	%PathFollow2D.progress = int(%PathFollow2D.progress)%326# length of progress
+	%PathFollow2D.progress = int(%PathFollow2D.progress) % 326 # length of progress
 	%doll.rotation += 10*ejection_speed
-	ejection_speed
--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
 func _ready():
-	%Player.global_position = %PlayerSpawn.global_position
-	%Player.frozen = false
 	#%DeathScreen.get_child(0).color = Color(0,0,0,0)
-	%DeathLabel.hide()
-	
+	#%DeathLabel.hide()
+	pass
+
 func _on_player_death(death_message: String):
 	%Player.frozen = true
 	%DeathLabel.text = death_message
 	
 	%DeathScreenAnimationPlayer.play("fade in")	
-
 	
 	await get_tree().create_timer(1.).timeout
 	
