4bd754cc78603c42d1ea234bfabf932b16ac3565
Tom Chauvel <tom.chauvel@proton.me>
2024-04-07 06:26:40 +0200

    Shaders (#51)
    * shaders
    
    * light
    
    * update
    
    * mask

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -1,11 +1,21 @@
 extends Node2D
+var shader = preload("res://scenes/nuit.gdshader")
+
 
 func _ready():
 	#%DeathScreen.get_child(0).color = Color(0,0,0,0)
 	#%DeathLabel.hide()
+	$House.set_day(true)
+	material.set_shader_parameter("day",true)
 	$HouseInteriorCamera.make_current()
 
+func deathloupe(death_message: String):
+	_on_player_death(death_message)
+	$House.set_day(false)
+	material.set_shader_parameter("day",false)
+
 func _on_player_death(death_message: String):
+	print(death_message)
 	%Player.frozen = true
 	%DeathLabel.text = death_message
 	
@@ -22,15 +32,15 @@ func _on_player_death(death_message: String):
 	%Player.get_node("Dino").rotation = 0
 
 	%Player.get_node("Dino").show()
-	
+
 	%Player.show()
 	%Player.frozen = false
 	await get_tree().create_timer(2.).timeout
-	
+
 	%DeathLabel.hide()
 	%DeathLabel.set("theme_override_colors/font_color",Color(255, 255, 255, 1))
 	print("passe")
-	
+
 
 func _on_interior_detect_area_entered(_area: Area2D) -> void:
 	# the player entered the house
