3b75e86d24c225fda3a7f518ddaa563470f6aea1
A1RONE <125598830+A1RONE@users.noreply.github.com>
2024-04-09 01:33:30 +0200

    design

--- scenes/hazards/block/block.gd
+++ scenes/hazards/block/block.gd
@@ -6,7 +6,7 @@ func action(player):
 	%Interact.hide()
 	player.frozen = true
 	player.get_node("Dino").play_deathblock_animation()
-	await get_tree().create_timer(.25).timeout
+	await get_tree().create_timer(1.5).timeout
 	%AnimationPlayer.play("mariojump")
 	
 	player_death.emit("Dieu n'aime pas que vous vous cassiez la tête.\nDieu a donc retiré les questions de ce monde.")
